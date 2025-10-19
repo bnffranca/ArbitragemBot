3f5d38821cbc63773a268da28a39fd75c7ef426c
bnffranca <bnffranca12@gmail.com>
2025-10-19 20:23:04 -0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import arbitragem_bot
 import time
 import requests
+import arbitragem_bot
 
 print("🟢 Bot de Arbitragem iniciado com sucesso!")
 
 while True:
     try:
-        arbitragem_bot.analisar_arbitragem()
+        arbitragem_bot.enviar_mensagem("✅ Bot ativo e monitorando oportunidades...")
+        time.sleep(60)
     except Exception as e:
         url = f"https://api.telegram.org/bot{arbitragem_bot.TELEGRAM_TOKEN}/sendMessage"
-        requests.post(url, data={"chat_id": arbitragem_bot.CHAT_ID, "text": f"Erro no bot: {e}"})
-    time.sleep(60)
+        data = {"chat_id": arbitragem_bot.CHAT_ID, "text": f"Erro no bot: {e}"}
+        requests.post(url, data=data)
+        time.sleep(60)
