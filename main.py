ef5259b751c7bb3f7f0233e177552ca1264c272a
bnffranca <bnffranca12@gmail.com>
2025-10-19 23:56:42 -0300

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
         arbitragem_bot.enviar_mensagem("✅ Bot ativo e monitorando oportunidades...")
         time.sleep(60)
     except Exception as e:
-        url = f"https://api.telegram.org/bot{arbitragem_bot.TELEGRAM_TOKEN}/sendMessage"
-        data = {"chat_id": arbitragem_bot.CHAT_ID, "text": f"Erro no bot: {e}"}
+        url = "https://api.telegram.org/bot8359395025:AAEq1HihEgoRFl5Fz6pnx2h30lFFLBPov10/sendMessage"
+        data = {"chat_id": "1809414360", "text": f"Erro no bot: {e}"}
         requests.post(url, data=data)
         time.sleep(60)
